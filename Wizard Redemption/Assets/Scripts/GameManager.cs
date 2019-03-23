d97eebbfcdfb272609e76d62b5b750a316117843
Javier Rodriguez Saiz <42693226+jarosa1990@users.noreply.github.com>
2019-03-23 20:06:44 +0100

    fdsf

---
+++ Wizard Redemption/Assets/Scripts/GameManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Enumerado para los distintos estados
+public enum GameState {
+
+    menu,
+    inGame,
+    gameOver
+}
+
+
+public class GameManager : MonoBehaviour {
+
+    //Variable que referencia al propio Game Manager
+    public static GameManager sharedInstance;
+
+    //En que estado del juego nos encontramos
+    public GameState currentGameState = GameState.menu;
+
+    private void Awake() {
+
+        sharedInstance = this;
+    }
+
+    public void Start() {
+
+        BackToMenu();
+    }
+
+    private void Update() {
+
+        if (Input.GetKeyDown(KeyCode.S)) {
+
+            StartGame();
+        }
+    }
+
+
+    //Inicia el juego
+    public void StartGame() {
+        SetGameState(GameState.inGame);
+
+    }
+
+    //Sera llamado cuando el jugador muera
+    public void GameOver() {
+        SetGameState(GameState.gameOver);
+
+    }
+
+    //Para volver al menu principal
+    public void BackToMenu() {
+        SetGameState(GameState.menu);
+
+    }
+
+    //Para cambiar el estado del juego
+    void SetGameState(GameState newGameState) {
+
+        if(newGameState == GameState.menu) {
+
+            //Hay que prepara la escena que muestra el menu
+        }else if(newGameState == GameState.inGame) {
+
+            //Hay que prepara la escena para jugar
+        } else if(newGameState == GameState.gameOver) {
+
+            //Hay que prepara la escena para el gameover
+        }
+
+        //Asignamos el estado del juego por parametro
+        this.currentGameState = newGameState;
+    }
+}
---
+++ Wizard Redemption/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    public float jumpForce = 30f;
+
+    public float runningSpeed = 1.5f;
+
+    public Animator animator;
+
+    private Rigidbody2D rigidbody;
+
+    public LayerMask groundLayer;
+
+
+    private void Awake() {
+
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+
+    // Start is called before the first frame update
+    void Start() {
+        animator.SetBool("isAlive", true);
+        animator.SetBool("isGrounded", true);
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if(GameManager.sharedInstance.currentGameState == GameState.inGame) {
+
+            Jump();
+
+            //Le decimos a la animacion si esta tocando el suelo o no con nuestro metodo IsTouchingTheGround()
+            animator.SetBool("isGrounded", IsTouchingTheGround());
+        }
+
+        
+    }
+
+    void FixedUpdate() {
+
+        if (GameManager.sharedInstance.currentGameState == GameState.inGame) {
+
+            Move();
+        }
+
+            
+    }
+
+    void Jump() {
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+
+            if (IsTouchingTheGround()) {
+                //F = m * a =======> a = F/m
+                rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }
+        }
+
+    }
+
+    void Move() {
+
+        if (Input.GetKey(KeyCode.D)) {
+
+            if (rigidbody.velocity.x < runningSpeed) {
+
+                rigidbody.velocity = new Vector2(runningSpeed,//Velocidad en el eje de las X
+                                                 rigidbody.velocity.y//Velocidad en el eje de las y
+                                                 );
+
+            }
+        }else if (Input.GetKey(KeyCode.A)) {
+
+            if (rigidbody.velocity.x < runningSpeed) {
+
+                rigidbody.velocity = new Vector2(-runningSpeed,//Velocidad en el eje de las X
+                                                 rigidbody.velocity.y//Velocidad en el eje de las y
+                                                 );
+
+            }
+        }
+
+            
+    }
+
+    void Walk() {
+
+
+    }
+
+
+    bool IsTouchingTheGround() {
+
+        if(Physics2D.Raycast(this.transform.position,//trazamos el rayo desde la posici'on del jugador 
+                             Vector2.down,//direccion hacia el suelo
+                             0.2f,//hasta un maximo de 20 cm
+                             groundLayer//y nos encontramos con la capa del suelo
+                             )) {
+            return true;
+
+        } else {
+
+            return false;
+        }
+    }
+}
